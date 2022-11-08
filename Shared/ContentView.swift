a523e78b8ceaaa5b8f96f8b6ca967aa3102c0e74
anhpham1 <anhpham1@tdt.asia>
2022-11-08 13:37:35 +0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by ECO0836_ANHPT on 08/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/NhatKyAnHanhApp.swift
@@ -0,0 +1,17 @@
+//
+//  NhatKyAnHanhApp.swift
+//  Shared
+//
+//  Created by ECO0836_ANHPT on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct NhatKyAnHanhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
