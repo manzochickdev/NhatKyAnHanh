b44be06a8484ee50f2ca1033841a445eb39ce873
anhpham1 <anhpham1@tdt.asia>
2022-11-08 15:45:47 +0700

    Add template note modify screen

--- Screens/NoteModifyScreen.swift
+++ Screens/NoteModifyScreen.swift
@@ -8,13 +8,66 @@
 import SwiftUI
 
 struct NoteModifyScreen: View {
+    @State var title:String = ""
+    @State var content:String = ""
+    @State var tagKeyword:String = ""
+    @State var listTag:[TagModel] = []
+    @State var shouldShowTagSuggestion:Bool = false
+    
     var body: some View {
-        Text("Note Modify")
+        VStack(alignment:.leading){
+            Button {
+                
+            } label: {
+                Text("Save")
+            }
+
+            Text("Title")
+            TextField("", text: $title)
+            
+            Text("Content")
+            TextView(text: $content)
+            
+            Text("Tag")
+            
+            Button {
+                shouldShowTagSuggestion = true
+            } label: {
+                HStack{
+                    Image(systemName: "plus")
+                    Text("Add Tag")
+                }
+            }.popover(isPresented: $shouldShowTagSuggestion,arrowEdge:.bottom) {
+                renderTagSearchView
+            }
+            
+        }
+        .padding()
+    }
+    
+    var renderTagSearchView : some View{
+        VStack{
+            TextField("", text: $tagKeyword)
+            List{
+                ForEach(["A","B","C"],id: \.self) { item in
+                    Text(item)
+                        .expandedWidth()
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            print(item)
+                        }
+                }
+            }
+        }
+        .padding()
+        .width(300)
+        .height(200)
     }
 }
 
 struct NoteModifyScreen_Previews: PreviewProvider {
     static var previews: some View {
         NoteModifyScreen()
+            .defaultFrame()
     }
 }
